da43c0cf6ab1a517ceb1aa4068e4a548b1ed29a5
Alina Osv <hixid@ya.ru>
2020-03-31 22:13:15 +0300

    Finished handemade redux

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 import './styles.css'
+import {rootReducer} from './redux/rootReducer';
 import {createStore} from './createStore';
 
-const store = createStore();
+const counter = document.getElementById('counter'); 
+const addBtn = document.getElementById('add');
+const subBtn = document.getElementById('sub');
+const asyncBtn = document.getElementById('async');
+const themeBtn = document.getElementById('theme');
+
+const store = createStore(rootReducer, 0);
+
+addBtn.addEventListener('click', () => {
+    store.dispatch({type: 'INCREMENT'});
+});
+subBtn.addEventListener('click', () => {
+    store.dispatch({type: 'DECREMENT'});
+});
+asyncBtn.addEventListener('click', ()=> {
+
+});
+/* themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark'); 
+}); */
+
+store.subscribe(() => {
+    const state = store.getState();
+    counter.textContent = state;
+});
+store.dispatch({type: 'INIT_APPLICATION'});
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,3 +1,7 @@
 export function rootReducer(state, action) {
+    if (action.type === 'INCREMENT') return state + 1;
+
+    if (action.type === 'DECREMENT') return state - 1;
+
     return state;
 }
